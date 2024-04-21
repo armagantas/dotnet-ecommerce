bf58e9a9c7de40917169019c25bae307f0466bed
Armağan Erdem Taş <65099385+armagantas@users.noreply.github.com>
2024-04-21 04:09:15 +0300

    Initial commit.

---
+++ dotnet-ecommerce/Services/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+
+namespace Catalog.API.Products.CreateProduct;
+public record CreateProductCommand(string Name, string Description, decimal Price, string ImageFile, int CategoryId, List<string> Category)
+    :IRequest<CreateProductResult>;
+public record CreateProductResult(Guid Id);
+internal class CreateProductHandler : IRequestHandler<CreateProductCommand, CreateProductResult>
+{
+    public Task<CreateProductResult> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
