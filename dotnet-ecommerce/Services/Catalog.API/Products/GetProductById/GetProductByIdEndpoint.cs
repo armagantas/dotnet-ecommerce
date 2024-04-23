b0034e38e769a7e3074446a15cdf8fd1a68c8460
Armağan Erdem Taş <65099385+armagantas@users.noreply.github.com>
2024-04-23 19:54:18 +0300

    GetProductById configs are done. Exception done.

---
+++ dotnet-ecommerce/Services/Catalog.API/Products/GetProductById/GetProductByIdEndpoint.cs
@@ -0,0 +1,26 @@
+﻿
+namespace Catalog.API.Products.GetProductById;
+
+//public record GetProductByIdQuery(Guid Id) : IQuery<GetProductByIdResult>;
+
+public record GetProductByIdResponse(Product Product);
+
+public class GetProductByIdEndpoint : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapGet("/products/{id}", async (Guid id, ISender sender) =>
+        {
+            var result = await sender.Send(new GetProductByIdQuery(id));
+
+            var response = result.Adapt<GetProductByIdResponse>();
+
+            return Results.Ok(response);
+        })
+        .WithName("GetProductById")
+        .Produces<GetProductByIdResponse>(StatusCodes.Status200OK)
+        .ProducesProblem(StatusCodes.Status400BadRequest)
+        .WithSummary("Get product by id")
+        .WithDescription("Returns a product by id");
+    }
+}
--- dotnet-ecommerce/Services/Catalog.API/Products/GetProducts/GetProductsQueryHandler.cs
+++ dotnet-ecommerce/Services/Catalog.API/Products/GetProducts/GetProductsQueryHandler.cs
@@ -7,11 +7,11 @@ public record GetProductsResult(IEnumerable<Product> Products);
 
 internal class GetProductsQueryHandler(IDocumentSession session, ILogger<GetProductsQueryHandler> logger) : IQueryHandler<GetProductsQuery, GetProductsResult>
 {
-    public Task<GetProductsResult> Handle(GetProductsQuery request, CancellationToken cancellationToken)
+    public async Task<GetProductsResult> Handle(GetProductsQuery request, CancellationToken cancellationToken)
     {
         logger.LogInformation("GetProductsQueryHandler.Handle called with {@Query}", request);
 
-        var products = session.Query<Product>().ToListAsync(cancellationToken);
+        var products = await session.Query<Product>().ToListAsync(cancellationToken);
 
         return new GetProductsResult(products);
     }
