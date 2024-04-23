2e4ba63280e87930cba30a50b11ad01015b87de2
Armağan Erdem Taş <65099385+armagantas@users.noreply.github.com>
2024-04-23 03:20:58 +0300

    Get product handler and endpoint done.

---
+++ dotnet-ecommerce/Services/Catalog.API/Products/GetProducts/GetProductsQueryHandler.cs
@@ -0,0 +1,18 @@
+﻿
+namespace Catalog.API.Products.GetProducts;
+
+public record GetProductsQuery : IQuery<GetProductsResult>;
+
+public record GetProductsResult(IEnumerable<Product> Products);
+
+internal class GetProductsQueryHandler(IDocumentSession session, ILogger<GetProductsQueryHandler> logger) : IQueryHandler<GetProductsQuery, GetProductsResult>
+{
+    public Task<GetProductsResult> Handle(GetProductsQuery request, CancellationToken cancellationToken)
+    {
+        logger.LogInformation("GetProductsQueryHandler.Handle called with {@Query}", request);
+
+        var products = session.Query<Product>().ToListAsync(cancellationToken);
+
+        return new GetProductsResult(products);
+    }
+}
