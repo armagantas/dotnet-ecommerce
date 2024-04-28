00639299fef9341492042faaa3dffcd619839a79
Armağan Erdem Taş <65099385+armagantas@users.noreply.github.com>
2024-04-28 04:12:00 +0300

    configurations and refactoring.

--- dotnet-ecommerce/Services/Basket/Basket.API/Basket/GetBasket/GetBasketEndpoint.cs
+++ dotnet-ecommerce/Services/Basket/Basket.API/Basket/GetBasket/GetBasketEndpoint.cs
@@ -1,6 +1,4 @@
-﻿
-
-namespace Basket.API.Basket.GetBasket;
+﻿namespace Basket.API.Basket.GetBasket;
 
 public record GetBasketResponse(ShoppingCart Cart);
 
@@ -8,7 +6,7 @@ public class GetBasketEndpoint : ICarterModule
 {
     public void AddRoutes(IEndpointRouteBuilder app)
     {
-        app.MapGet("/basket/{userName", async (string userName, ISender sender) =>
+        app.MapGet("/basket/{userName}", async (string userName, ISender sender) =>
         {
             var result = await sender.Send(new GetBasketQuery(userName));
 
