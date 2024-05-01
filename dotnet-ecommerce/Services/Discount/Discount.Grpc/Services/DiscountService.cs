f32001857aaa44f3976bbd4003cc856b9f5a7b7d
Armağan Erdem Taş <65099385+armagantas@users.noreply.github.com>
2024-05-01 20:13:37 +0300

    Discount Service, dbContext file, extentions and some configs done.

---
+++ dotnet-ecommerce/Services/Discount/Discount.Grpc/Services/DiscountService.cs
@@ -0,0 +1,31 @@
+﻿using Grpc.Core;
+
+namespace Discount.Grpc.Services;
+
+public class DiscountService : DiscountProtoService.DiscountProtoServiceBase
+{
+    public override Task<CouponModel> GetDiscount(GetDiscountRequest request, ServerCallContext context)
+    {
+        // Get the discount from the repository
+        return base.GetDiscount(request, context);
+    }
+
+    public override Task<CouponModel> CreateDiscount(CreateDiscountRequest request, ServerCallContext context)
+    {
+        // Create the discount in the repository
+        return base.CreateDiscount(request, context);
+    }
+
+    public override Task<CouponModel> UpdateDiscount(UpdateDiscountRequest request, ServerCallContext context)
+    {
+        // Update the discount in the repository
+        return base.UpdateDiscount(request, context);
+    }
+
+    public override Task<DeleteDiscountResponse> DeleteDiscount(DeleteDiscountRequest request, ServerCallContext context)
+    {
+        // Delete the discount from the repository
+        return base.DeleteDiscount(request, context);
+    }
+
+}
