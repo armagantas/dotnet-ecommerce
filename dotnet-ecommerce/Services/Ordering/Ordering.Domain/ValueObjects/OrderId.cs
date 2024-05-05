f2ae6cbf526b7e980b21c335c8027fb63e5b77c8
Armağan Erdem Taş <65099385+armagantas@users.noreply.github.com>
2024-05-06 02:26:55 +0300

    Developed general exception and ValueObjects DDD Rich domain model entity.

--- dotnet-ecommerce/Services/Ordering/Ordering.Domain/ValueObjects/OrderId.cs
+++ dotnet-ecommerce/Services/Ordering/Ordering.Domain/ValueObjects/OrderId.cs
@@ -3,4 +3,16 @@
 public class OrderId
 {
     public Guid Value { get; }
+    private OrderId(Guid value) => Value = value;
+
+    public static OrderId Of(Guid value)
+    {
+        ArgumentNullException.ThrowIfNull(value);
+        if(value == Guid.Empty)
+        {
+            throw new DomainException("Order id cannot be empty");
+        }
+
+        return new OrderId(value);
+    }
 }
