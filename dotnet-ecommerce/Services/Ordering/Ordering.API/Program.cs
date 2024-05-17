cab40ea064f789cd7ba1309038009e7fdbdd3537
Armağan Erdem Taş <65099385+armagantas@users.noreply.github.com>
2024-05-17 20:51:04 +0300

    HealthCheck done.

--- dotnet-ecommerce/Services/Ordering/Ordering.API/Program.cs
+++ dotnet-ecommerce/Services/Ordering/Ordering.API/Program.cs
@@ -6,7 +6,7 @@ using Ordering.Infrastructure.Data.Extentions;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddApplicationServices().AddInfrastructureServices(builder.Configuration).AddApiServices();
+builder.Services.AddApplicationServices().AddInfrastructureServices(builder.Configuration).AddApiServices(builder.Configuration);
 
 var app = builder.Build();
 
