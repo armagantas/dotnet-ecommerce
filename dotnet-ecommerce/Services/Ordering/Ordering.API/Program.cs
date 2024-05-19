97a6875ecb3a4da754965c7712979e1fce7b3c34
Armağan Erdem Taş <65099385+armagantas@users.noreply.github.com>
2024-05-20 02:43:48 +0300

    Project reference added to the Ordering.Application , and configurations are done.

--- dotnet-ecommerce/Services/Ordering/Ordering.API/Program.cs
+++ dotnet-ecommerce/Services/Ordering/Ordering.API/Program.cs
@@ -6,7 +6,7 @@ using Ordering.Infrastructure.Data.Extentions;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddApplicationServices().AddInfrastructureServices(builder.Configuration).AddApiServices(builder.Configuration);
+builder.Services.AddApplicationServices(builder.Configuration).AddInfrastructureServices(builder.Configuration).AddApiServices(builder.Configuration);
 
 var app = builder.Build();
 
