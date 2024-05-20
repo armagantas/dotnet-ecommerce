13b49f3c6d26356ea3ec886ed39c23f24ffb44b2
Armağan Erdem Taş <65099385+armagantas@users.noreply.github.com>
2024-05-20 11:10:59 +0300

    EventHandler folder structure has been changed. New method added.

---
+++ dotnet-ecommerce/Services/Ordering/Ordering.Application/Orders/EventHandlers/Integration/BasketCheckoutEventHandler.cs
@@ -0,0 +1,17 @@
+using BuildingBlocks.Messaging.Events;
+using MassTransit;
+using MediatR;
+using Microsoft.Extensions.Logging;
+
+namespace Ordering.Application.Orders.EventHandlers.Integration;
+
+public class BasketCheckoutEventHandler(ISender sender, ILogger<BasketCheckoutEventHandler> logger) : IConsumer<BasketCheckoutEvent>
+{
+    public async Task Consume(ConsumeContext<BasketCheckoutEvent> context)
+    {
+        logger.LogInformation("Integration event handled: {IntegrationEvent}", context.Message.GetType().Name);
+
+        var command = MapToCreateOrderCommand(context.Message);
+        await sender.Send(command);
+    }
+}
